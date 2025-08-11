18fa0fdb2d3ecc966fd4c034d5fe56b0b2bf9543
Stef <stbuwalda@gmail.com>
2025-08-11 16:56:40 +0200

    Remove admin food item and barcode test routes and templates
    Deleted the /food_items and /barcode_test routes from admin, along with their associated templates and the delete_food functionality. This streamlines the admin blueprint by removing unused or deprecated features.

--- application/admin/routes.py
+++ application/admin/routes.py
@@ -1,7 +1,5 @@
-from flask import Blueprint, render_template, abort, redirect, url_for
+from flask import Blueprint, abort
 from flask_login import current_user
-from models import FoodItem
-from application import db
 
 admin_bp = Blueprint(
     "admin",
@@ -15,24 +13,3 @@ admin_bp = Blueprint(
 def admin_required():
     if not current_user.is_admin:
         abort(403)
-
-
-@admin_bp.route("/food_items", methods=["GET"])
-def food_items():
-    items = FoodItem.query.all()
-    return render_template("food_items.html", items=items)
-
-
-@admin_bp.route("/barcode_test", methods=["GET"])
-def barcode_test():
-    return render_template("barcode_test.html")
-
-
-@admin_bp.route("/delete_food/<int:id>", methods=["POST"])
-def delete_food(id):
-    item = FoodItem.query.get(id)
-    if item:
-        if item.owner_id == current_user.id:
-            db.session.delete(item)
-            db.session.commit()
-    return redirect(url_for("admin.food_items"))
